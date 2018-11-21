99107e5584a29bcf6ba38c045f09d15e1ebc9901
Reo Hirao <reohirao116@gmail.com>
2018-11-21 21:01:17 +0900

    ちょっと更新

--- pythonFile/distance.py
+++ pythonFile/distance.py
@@ -1,7 +1,7 @@
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
+# For example, here's several helpful packages to load in
+#%%
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
@@ -10,30 +10,46 @@ import math
 from sympy import isprime
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
 import os
+# print(os.getcwd())
+# os.chdir('pythonFile')
+DEBUG = False
+DEBUG_SIZE = 10
 print(os.listdir("../input"))
-
 # Any results you write to the current directory are saved as output.
 
 cities_df = pd.read_csv('../input/cities.csv')
 sample_submission = pd.read_csv('../input/sample_submission.csv')
 
-cities_df.head(10)
-
+if DEBUG:
+    cities_df = cities_df[:DEBUG_SIZE]
+    sample_submission = sample_submission[:DEBUG_SIZE]
+cities_df = cities_df.sample(frac=1)
+cities_df.head(15)
 cities_df.describe()
-
-figure = plt.figure(figsize=(15,15))
+figure = plt.figure(figsize=(8,8))
 plt.scatter(cities_df['X'], cities_df['Y'], 1)
-
 sample_submission.head(10)
-
 len(cities_df)
-
 cities_df.info()
 
-# The function to get the distance between the cities.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Score Calculation
+# The function to get the distance between the cities.
 def distance(x1, y1, x2, y2, prev_is_prime, is_10th):
     # Every 10th step is 10% more lengthy unless coming from a prime CityId.
     cost_factor = 1.1 if is_10th and not prev_is_prime else 1.0
@@ -41,7 +57,6 @@ def distance(x1, y1, x2, y2, prev_is_prime, is_10th):
 
 # The function to calculate score.
 # The beginning and end of the paths must be City'0'.
-
 def calculate_score(paths, cities_df):
     cities_df['IsPrime'] = cities_df['CityId'].apply(isprime)
     cities_df_dict = cities_df.to_dict()
